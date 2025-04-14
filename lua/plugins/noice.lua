a830f01abb827ff0badf5281cc984d15238532f4
Johnson Cheung <johnson@revlyx.com>
2025-04-14 03:22:35 -0700

    added noice lua

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,27 @@
+
+
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
+  opts = {
+    lsp = {
+      progress = {
+        enabled = false,
+      },
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+      },
+    },
+    presets = {
+      bottom_search = false,
+      command_palette = false,
+      long_message_to_split = false,
+      inc_rename = false,
+    },
+  },
+}
