efa8736a707fe4897a47736ecc6de8da3bfbfb02
Johnson Cheung <johnson@revlyx.com>
2025-04-28 12:52:51 -0700

    added mcp

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -10,6 +10,12 @@ return {
         strategies = {
           chat = {
             adapter = "copilot",
+            tools = {
+              ["mcp"] = {
+                callback = function() return require("mcphub.extension.codecompanion") end,
+                description = "Call tools and resources for mcp servers"
+              }
+            }
           },
           inline = {
             adapter = "copilot",
